9484fe38790d10dea1836aa7d4794d5ba016575c
21Tito <theoyeh@gmail.com>
2024-12-18 21:33:07 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // You can add your JavaScript code here
+    console.log('Website loaded successfully!');
+});
